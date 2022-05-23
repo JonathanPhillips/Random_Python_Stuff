e54194af06d4562d3d4089498b563035d412fce5
JonathanPhillips <jon7035@gmail.com>
2022-05-23 17:55:06 -0400

    Create csv.py

---
+++ filesanddirs/csv.py
@@ -0,0 +1,101 @@
+import csv
+f = open("csv_file.txt")
+csv_f = csv.reader(f)
+for row in csv_f:
+	name, phone, role = row
+	print("Name: {}, Phone: {}, Role: {}".format(name, phone, role))
+f.close()
+
+
+#generate csv
+hosts = [["workstation.local", "192.168.25.46"], ["webserver.cloud", "192.168.5.6"]]
+with open('hosts.csv', 'w') as hosts_csv:
+	writer = csv.writer(hosts_csv)
+	writer.writerows(hosts)
+
+
+with open('software.csv') as software:
+	reader = csv.DictReader(software)
+	for row in reader:
+		print(("{} has {} users").format(row["name"], row["users"]))
+
+
+
+
+
+'''
+We're working with a list of flowers and some information about each one. The create_file function writes this information to a CSV file. The contents_of_file function reads this file into records and returns the information in a nicely formatted block. Fill in the gaps of the contents_of_file function to turn the data in the CSV file into a dictionary using DictReader.
+'''
+
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  with open(filename) as flowers:
+    # Read the rows of the file into a dictionary
+    reader = csv.DictReader(flowers)
+    # Process each item of the dictionary
+    for row in reader:
+      return_string += "a {} {} is {}\n".format(row["color"], row["name"], row["type"])
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
+
+'''
+Using the CSV file of flowers again, fill in the gaps of the contents_of_file function to process the data without turning it into a dictionary. How do you skip over the header record with the field names?
+'''
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  with open(filename) as flowers:
+    # Read the rows of the file
+    rows = csv.reader(flowers)
+    next(rows)
+    # Process each row
+    for row in rows:
+      name, color, type = row
+      # Format the return string for data rows only
+
+      return_string += "a {} {} is {}\n".format(name, color, type)
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
+
